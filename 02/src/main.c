9f79e605fab31d02379e74825f6f78e77fd15e58
Mikayla Fischler <mikaylafischler@gmail.com>
2019-12-03 17:33:23 -0500

    day 2 files were swapped

--- 02/src/main.c
+++ 02/src/main.c
@@ -5,9 +5,56 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "../lib/intcode/intcode.h"
-
 #include "main.h"
+
+#define MEM_SIZE 1000
+
+#define IC_OP__ADD 1
+#define IC_OP__MUL 2
+#define IC_OP__HLT 99
+
+#define IC_INC_PC(pc) (pc = pc + 4)
+
+typedef struct instruction {
+	int32_t op;
+	int32_t a;
+	int32_t b;
+	int32_t dest;
+} instr_t;
+
+/**
+ * @brief Execute a series of intcode instructions in provided memory
+ *
+ * @param memory Initial memory to be modified by execution
+ * @return uint8_t 1 if halted, 0 if error
+ */
+uint8_t intcode_compute(int32_t* memory) {
+	uint8_t  halt 	= 0;
+	uint16_t pc 	= 0;
+
+	do {
+		instr_t* instr = (instr_t*) (memory + pc);
+
+		switch (instr->op) {
+			case IC_OP__ADD:
+				memory[instr->dest] = memory[instr->a] + memory[instr->b];
+				break;
+			case IC_OP__MUL:
+				memory[instr->dest] = memory[instr->a] * memory[instr->b];
+				break;
+			case IC_OP__HLT:
+				halt = 1;
+				break;
+			default:
+				printf(B_RED ">" RESET " Intcode: illegal OP code at instruction %d\n", pc);
+				return 0;
+		}
+
+		IC_INC_PC(pc);
+	} while (!halt && pc < MEM_SIZE - 4);
+
+	return halt;
+}
 
 int main(int argc, char** argv) {
     printf(WHITE "x------------------------------------------------x\n" RESET);
@@ -36,27 +83,48 @@ int main(int argc, char** argv) {
 	int32_t p2_res = 0;
 
 	// setup intcode computer
+	uint16_t pc = 0;
+	int32_t* memory 	= malloc(sizeof(int32_t) * MEM_SIZE);
+	int32_t* memory_bkp	= malloc(sizeof(int32_t) * MEM_SIZE);
+
 	printf(YELLOW ">" B_WHITE " initializing memory...\n" RESET);
 	mem_start = clock();
 
-	icd_t* icdata;
-	intcode_memory__load_file(fp, icdata);
-	intcode_memory__backup(icdata);
+	// read in memory
+	while ((num_read = getline(&line, &len, fp)) != -1) {
+		// tokenize line, delimited by comma
+		char* token = strtok(line, ",");
+		while (token != NULL) {
+			memory[pc++] = atoi(token);
+
+			if (memory[pc - 1] == 0 && token[0] != '0') {
+				// bad input, skip
+				pc--;
+				printf(B_YELLOW ">" RESET " failed to parse \"%s\" as integer input\n", token);
+			}
+
+			token = strtok(NULL, ",");
+		}
+	}
+
+	// backup initial configuration
+	memcpy(memory_bkp, memory, sizeof(int32_t) * MEM_SIZE);
 
 	mem_end = clock();
 	printf(B_GREEN ">" B_WHITE " memory initialized " WHITE "(" BLUE "%.3f ms" WHITE ")\n" RESET, (mem_end - mem_start) * 1000.0 / CLOCKS_PER_SEC);
 
 	// load in values for part 1
-	intcode_load_init(icdata, 12, 2);
+	memory[1] = 12;
+	memory[2] = 2;
 
 	// calculate result for part 1
 	printf(YELLOW ">" B_WHITE " executing part 1...\n" RESET);
 	p1_start = clock();
 
-	if (intcode_compute(icdata)) {
+	if (intcode_compute(memory)) {
 		p1_end = clock();
 		printf(B_GREEN ">" B_WHITE " part 1 execution succeeded " WHITE "(" BLUE "%lu us" WHITE ")\n" RESET, (p1_end - p1_start) * 1000000 / CLOCKS_PER_SEC);
-		p1_res = intcode_result(icdata);
+		p1_res = memory[0];
 	} else {
 		p1_end = clock();
 		printf(B_RED ">" B_WHITE " part 1 execution failed " WHITE "(" BLUE "%lu us" WHITE ")\n" RESET, (p1_end - p1_start) * 1000000 / CLOCKS_PER_SEC);
@@ -70,16 +138,18 @@ int main(int argc, char** argv) {
 	uint8_t run = 1;
 	for (int a = 0; a <= 99 && run; a++) {
 		for (int b = 0; b <= 99 && run; b++) {
-			intcode_memory__restore(icdata);
-			intcode_load_init(icdata, a, b);
+			memcpy(memory, memory_bkp, 1000 * sizeof(int));
 
-			if (intcode_compute(icdata) == 0) {
+			memory[1] = a;
+			memory[2] = b;
+
+			if (intcode_compute(memory) == 0) {
 				printf(RED ">" B_WHITE " intcode execution failed for (noun,verb) pair (%d,%d)", a, b);
 			}
 
-			if (intcode_result(icdata) == 19690720) {
+			if (memory[0] == 19690720) {
 				p2_end = clock();
-				p2_res = a * 100 + b;
+				p2_res = memory[1] * 100 + memory[2];
 				run = 0;
 			}
 		}
